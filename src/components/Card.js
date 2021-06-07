619b163891e915cba128b812b5b5afea37e505bf
Justinas Kairys <info@ibnz.co.uk>
2021-06-07 12:10:23 +0100

    fixed rel error

--- src/components/Card.js
+++ src/components/Card.js
@@ -69,19 +69,19 @@ const Card = () => {
   };
 
   const fetchAddressfromTronlink = async () => {
-    setmyAddress('Loading...')
+    setmyAddress('Loading...');
     try {
       setTimeout(() => {
-        if(window.tronWeb) {
-
+        if (window.tronWeb) {
           setmyAddress(window.tronWeb.defaultAddress.base58);
-        } else {setmyAddress(null);}
+        } else {
+          setmyAddress(null);
+        }
       }, 1000);
     } catch (error) {
       console.error('not able to fetch ', error);
       setmyAddress(error);
     }
-
   };
 
   const getContractName = async () => {
@@ -172,8 +172,6 @@ const Card = () => {
     }
   };
 
-
-
   return (
     <div className={classes.cardGrid}>
       <div className={classes.first_border}></div>
@@ -188,7 +186,17 @@ const Card = () => {
           </div>
           <h1>SMART CONTRACT GUI</h1>
         </div>
-        {myAddress ? <p className={classes.myAddress}>Network (<b>{network}</b>): {myAddress}</p> : <p className={classes.myAddress}><a href="https://chrome.google.com/webstore/detail/tronlink%EF%BC%88%E6%B3%A2%E5%AE%9D%E9%92%B1%E5%8C%85%EF%BC%89/ibnejdfjmmkpcnlpebklmnkoeoihofec" >TRONLINK NOT DETECTED</a></p>}
+        {myAddress ? (
+          <p className={classes.myAddress}>
+            Network (<b>{network}</b>): {myAddress}
+          </p>
+        ) : (
+          <p className={classes.myAddress}>
+            <a href="https://chrome.google.com/webstore/detail/tronlink%EF%BC%88%E6%B3%A2%E5%AE%9D%E9%92%B1%E5%8C%85%EF%BC%89/ibnejdfjmmkpcnlpebklmnkoeoihofec" target="_blank" rel="noreferrer">
+              TRONLINK NOT DETECTED
+            </a>
+          </p>
+        )}
 
         <div className={classes.content}>
           <div>
@@ -228,11 +236,10 @@ const Card = () => {
           <button onClick={getContractName} className={classes.contrctButton}>
             Get smart contract details
           </button>
-          {/* <p>TPjGUuQfq6R3FMBmsacd6Z5dvAgrD2rz4n</p>
+          <p>TPjGUuQfq6R3FMBmsacd6Z5dvAgrD2rz4n</p>
           <p>TEvrLVLkcDpnSZb9G6AwVnWAR91SbTLBa1</p>
           <p>TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t</p>
-          <div></div> */}
-
+          <div></div>
 
           <TronlinkFunctions clicked={tronlinkTest} />
         </div>
